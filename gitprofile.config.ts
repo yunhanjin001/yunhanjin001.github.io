d2c21252bb057bafa33d9a1d4a308e979b8394b0
Yunhan Jin <96060621+yunhanjin001@users.noreply.github.com>
2025-10-31 10:02:23 -0400

    Disable display of GitHub projects

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -12,7 +12,7 @@ const CONFIG = {
   base: '/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
+      display: false, // Display GitHub projects?
       header: 'Github Projects',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
